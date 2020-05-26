411cb3d644380a68fade18dea0198fb714379eb8
klisiewicz <karol.lisiewicz@gmail.com>
2020-05-26 13:36:54 +0200

    Change: CurrencyRateEvent to use Sealed Class.

--- lib/src/feature/rates/view/bloc/currency_rate_bloc.dart
+++ lib/src/feature/rates/view/bloc/currency_rate_bloc.dart
@@ -17,15 +17,17 @@ class CurrencyRatesBloc extends Bloc<CurrencyRateEvent, CurrencyRateState> {
   @override
   CurrencyRateState get initialState => const CurrencyRatesLoading();
 
+  List<CurrencyRate> get _loadedRates =>
+      (state as CurrencyRatesLoaded)?.rates ?? [];
+
   void loadCurrencyRates() => add(const CurrencyRatesLoadEvent());
 
   @override
   Stream<CurrencyRateState> mapEventToState(CurrencyRateEvent event) async* {
-    if (event is CurrencyRatesLoadEvent) {
-      yield* _loadCurrencyRates();
-    } else if (event is CurrencyRatesRefreshEvent) {
-      yield* _refreshCurrencyRatesWhenLoaded();
-    }
+    yield* event.join<Stream<CurrencyRateState>>(
+      (loading) => _loadCurrencyRates(),
+      (refreshing) => _refreshCurrencyRates(),
+    );
   }
 
   Stream<CurrencyRateState> _loadCurrencyRates() async* {
@@ -46,23 +48,15 @@ class CurrencyRatesBloc extends Bloc<CurrencyRateEvent, CurrencyRateState> {
     );
   }
 
-  Stream<CurrencyRateState> _refreshCurrencyRatesWhenLoaded() async* {
-    final currentState = state;
-    if (currentState is CurrencyRatesLoaded) {
-      yield* _refreshCurrencyRates(currentState.rates);
-    }
-  }
-
-  Stream<CurrencyRateState> _refreshCurrencyRates(
-    List<CurrencyRate> loadedRates,
-  ) async* {
+  Stream<CurrencyRateState> _refreshCurrencyRates() async* {
     try {
-      yield CurrencyRatesRefreshing(loadedRates);
+      if (state is! CurrencyRatesLoaded) return;
+      yield CurrencyRatesRefreshing(_loadedRates);
       final List<CurrencyRate> refreshedRates =
           await _repository.getCurrencyRates();
       yield CurrencyRatesLoaded(refreshedRates);
     } catch (e) {
-      yield CurrencyRatesLoaded(loadedRates);
+      yield CurrencyRatesLoaded(_loadedRates);
     }
   }
 
